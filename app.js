f17dd8f5101a249489258a1d0e03c7b5eed41559
harshil-mistry <harshilmistry31@gmail.com>
2025-07-15 14:02:28 +0530

    Simple routing

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname+"/demo.html")
+    res.sendFile(__dirname+"/home.html")
+})
+
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname+"/about.html")
+})
+
+app.get('/contact', (req, res)=>{
+    res.sendFile(__dirname+"/contact.html")
 })
 
 app.listen(port, ()=>{
