b55740959432e3e5959b510c1123a5fc10204ab3
harshil-mistry <harshilmistry31@gmail.com>
2025-07-22 13:40:34 +0530

    Demo EJS implemented

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
-app.use(express.static('public'))
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true })) 
+
+app.set('views', __dirname+'/views')
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+"/home.html")
+})
+
+app.get('/sum', (req, res) => {
+    res.sendFile(__dirname + '/sum.html')
+})
+
+app.get('/ejs', (req, res)=>{
+    res.render('home.ejs', {'name':'Harshil'})
+})
+
+app.get('/processsum', (req, res) => {
+    num1 = req.query.num1
+    num2 = req.query.num2
+    sum = parseInt(num1) + parseInt(num2)
+    res.send(`The sum for ${num1} & ${num2} is : ${sum}`)
 })
 
 app.get('/about', (req, res)=>{
